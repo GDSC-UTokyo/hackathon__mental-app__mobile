61cb4ffd73bc722f3991ac6213353e95d680ff08
Arupaka1152 <hkohama1152@gmail.com>
2023-03-27 08:54:08 +0900

    create reason page

--- lib/screens/reason.dart
+++ lib/screens/reason.dart
@@ -1,6 +1,9 @@
+import 'package:app/screens/create_reason.dart';
+import 'package:app/screens/edit_reason.dart';
 import 'package:flutter/material.dart';
 import 'package:app/theme/theme.dart';
 import 'package:provider/provider.dart';
+import 'package:app/provider/reason.dart';
 
 class ReasonPage extends StatefulWidget {
   const ReasonPage({super.key});
@@ -13,12 +16,83 @@ class _ReasonPageState extends State<ReasonPage> {
   @override
   Widget build(BuildContext context) {
     final themeColor = Provider.of<ThemeProvider>(context).theme.colorTheme;
+    List<Reason> reasonList = context.watch<ReasonsProvider>().reasons;
 
     return Scaffold(
       backgroundColor: themeColor.background,
       appBar: AppBar(
         title: const Text('Reason'),
         backgroundColor: themeColor.primary,
+      ),
+      body: reasonList.isNotEmpty
+        ? Padding(
+          padding: const EdgeInsets.only(
+            top: 8,
+          ),
+          child: ListView.builder(
+            itemCount: reasonList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      reasonList[index].reason,
+                      style: const TextStyle(
+                        //fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          color: Colors.green,
+                          onPressed: () => {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) {
+                                return const EditReasonPage();
+                              }),
+                            ),
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () => {
+                            context.read<ReasonsProvider>().delete(reasonList[index].id)
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
+          )
+        )
+        : const Center(
+          child: Text(
+            "no report",
+            style: TextStyle(fontSize: 32, color: Colors.grey),
+            ),
+        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: themeColor.primary,
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return const CreateReasonPage();
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
